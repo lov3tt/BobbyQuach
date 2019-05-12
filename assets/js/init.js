333c3b8cc68e3d74d4e74940178b31ee381afcd3
Bobby Quach <BobbyQuach90@gmail.com>
2019-05-11 20:26:14 -0400

    finish

---
+++ assets/js/init.js
@@ -0,0 +1,34 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+    
+
+// var instance = M.Carousel.init({
+//   fullWidth: true,
+//   indicators: true
+// });
+
+// // Or with jQuery
+
+// $('.carousel.carousel-slider').carousel({
+//   fullWidth: true,
+//   indicators: true
+// });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.slider');
+  var instances = M.Slider.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.slider').slider();
+});
